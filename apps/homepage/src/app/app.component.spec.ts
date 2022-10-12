9b0eeefd9d667530d91ef057e946dbc33bdfc327
homeroET <h.escobartorres@be-tse.it>
2022-10-12 17:52:23 +0200

    init new configuration and added apps [login, homepage, commSuite]

--- apps/homepage/src/app/app.component.spec.ts
+++ apps/homepage/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -14,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'orchestrator'`, () => {
+  it(`should have as title 'homepage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('orchestrator');
+    expect(app.title).toEqual('homepage');
   });
 
   it('should render title', () => {
@@ -25,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome orchestrator'
+      'Welcome homepage'
     );
   });
 });
